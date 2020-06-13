39c116f7f0c4afc71c4d315e33c961c5b2efe557
KaiqueSMendonca <kaique.mendonca@etec.sp.gov.br>
2020-06-13 14:17:53 -0300

    C em F parte 2

---
+++ src/cemfpt2/CemFPt2.java
@@ -0,0 +1,16 @@
+package cemfpt2;
+
+public class CemFPt2 {
+
+
+    public static void main(String[] args) {
+        int i = 10;
+        do{
+            double f = 9*i/5+32;
+            System.out.println(i+" = "+f);
+            i += 10;
+        }while (i <= 100);
+              
+    }
+    
+}
